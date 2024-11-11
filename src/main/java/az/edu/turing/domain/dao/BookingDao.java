3e517c10b2e34f1249bc878edb86675d2b776cde
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-11 15:55:28 +0400

    modify BookingDao to reference correct entity package

--- src/main/java/az/edu/turing/domain/dao/BookingDao.java
+++ src/main/java/az/edu/turing/domain/dao/BookingDao.java
@@ -1,6 +1,6 @@
 package az.edu.turing.domain.dao;
 
-import az.edu.turing.entities.BookingEntity;
+import az.edu.turing.domain.entity.BookingEntity;
 
 public abstract class BookingDao implements Dao<BookingEntity,Long> {
 }
