209bd1a338f0ad88a3770b50f1509fe99fc55a6c
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-18 20:04:50 +0400

    bugfix/booking123: remove unnecessary blank lines in BookingService

--- src/main/java/az/edu/turing/service/BookingService.java
+++ src/main/java/az/edu/turing/service/BookingService.java
@@ -4,7 +4,6 @@ import az.edu.turing.model.dto.BookingDto;
 import az.edu.turing.model.dto.request.CreateBookingRequest;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BookingService {
 
