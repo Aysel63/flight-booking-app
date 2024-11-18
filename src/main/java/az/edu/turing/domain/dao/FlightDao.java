92b23197114ed17a646edaa95a8a268499689857
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-18 20:11:24 +0400

    bugfix/booking123: remove unnecessary blank lines in FlightDao

--- src/main/java/az/edu/turing/domain/dao/FlightDao.java
+++ src/main/java/az/edu/turing/domain/dao/FlightDao.java
@@ -5,6 +5,7 @@ import az.edu.turing.domain.entities.FlightEntity;
 import java.util.List;
 
 public abstract class FlightDao implements Dao<FlightEntity, Long> {
+
     public abstract FlightEntity updateAvailableSeats(long flightId, int newAvailableSeatCount);
 
     public abstract List<FlightEntity> getAllFlightsWithin24Hours();
